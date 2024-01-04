040c520b676b8b5c9d177fe00bdd0f02218d4284
Sam-Nyalik <samnyalik@gmail.com>
2024-01-04 10:53:04 +0300

    Page routes. 'home.php' is the default homepage

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
-
+<!-- ROUTES -->
 <?php 
+
+session_start();
 
 // Functions
 include_once "./functions/functions.php";
+$pdo = databaseConnection();
 
 // Page routes (Making home.php the dafult page)
 $page = isset($_GET['page']) && file_exists($_GET['page'] . '.php') ? $_GET['page'] : 'home';
