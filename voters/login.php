d670046041c8778bd5393c1214d644a291f04e08
Sam-Nyalik <samnyalik@gmail.com>
2024-01-06 00:50:51 +0300

    User login script

---
+++ voters/login.php
@@ -0,0 +1,139 @@
+<!-- VOTER LOGIN -->
+
+<?php
+
+session_start();
+
+// Error handling
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Functions & database connection
+include_once "./functions/functions.php";
+$pdo = databaseConnection();
+
+// Define variables and assign them empty values
+$emailAddress = $password = "";
+$emailAddress_error = $password_error = "";
+
+// Process form data when the form has been submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Validate emailAddress
+    if (empty(trim($_POST["emailAddress"]))) {
+        $emailAddress_error = "Field is required!";
+    } else {
+        $emailAddress = trim($_POST["emailAddress"]);
+    }
+
+    // Validate password
+    if (empty(trim($_POST["password"]))) {
+        $password_error = "Field is required!";
+    } else {
+        $password = trim($_POST["password"]);
+    }
+
+    // Check for errors before dealing with the database
+    if (empty($emailAddress_error) && empty($password_error)) {
+        // Prepare a SELECT statement
+        $sql = "SELECT voterId, emailAddress, password FROM voters WHERE emailAddress = :emailAddress";
+        if ($stmt = $pdo->prepare($sql)) {
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(":emailAddress", $param_emailAddress, PDO::PARAM_STR);
+            // Set parameters
+            $param_emailAddress = $emailAddress;
+            // Attempt to execute
+            if ($stmt->execute()) {
+                // Check if the user with the input emailAddress exists
+                if ($stmt->rowCount() == 1) {
+                    //User exists
+                    if ($row = $stmt->fetch()) {
+                        $id = $row['voterId'];
+                        $emailAddress = $row['emailAddress'];
+                        $hashed_password = $row['password'];
+
+                        // Verify the password
+                        if (password_verify($password, $hashed_password)) {
+                            // Passwords match
+                            session_start();
+
+                            // Store data in session variables
+                            $_SESSION["loggedIn"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["emailAddress"] = $emailAddress;
+
+                            // Redirect user to the dashboard page
+                            header("location: index.php?page=voters/dashboard");
+                            exit;
+                        } else {
+                            $password_error = "Wrong password!";
+                        }
+                    }
+                } else {
+                    $emailAddress_error = "User does not exist!";
+                }
+            } else {
+                echo "There was an error. Please try again!";
+            }
+        }
+
+        unset($stmt);
+    }
+}
+
+?>
+
+<!-- Header Template -->
+<?= headerTemplate('VOTER | LOGIN'); ?>
+
+<!-- Navbar Template -->
+<?= navbarTemplate(); ?>
+
+<!-- Breadcrumb -->
+<div class="breadcrumb">
+    <div class="container">
+        <div class="row">
+            <span><a href="index.php?page=home">Home</a> > Voter Login</span>
+        </div>
+    </div>
+</div>
+
+<!-- Login Section -->
+<div class="form">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5">
+                <!-- Title -->
+                <h3>Voter Login</h5>
+                    <p>Login to cast your vote today</p>
+                    <hr>
+
+                    <!-- Login Form -->
+                    <form action="index.php?page=voters/login" method="post" class="login-form">
+                        <!-- Email Address -->
+                        <div class="form-group my-3">
+                            <label for="EmailAddress">Email Address</label>
+                            <input type="email" name="emailAddress" class="form-control <?php echo (!empty($emailAddress_error)) ? 'is-invalid' : ''; ?>" value="<?php echo $emailAddress; ?>">
+                            <span class="errors text-danger"><?php echo $emailAddress_error; ?></span>
+                        </div>
+
+                        <!-- Password -->
+                        <div class="form-group my-3">
+                            <label for="password">Password</label>
+                            <input type="password" name="password" class="form-control <?php echo (!empty($password_error)) ? 'is-invalid' : ''; ?>">
+                            <span class="errors text-danger"><?php echo $password_error; ?></span>
+                        </div>
+
+                        <!-- Registration link -->
+                        <div class="form-group my-3">
+                            <a href="index.php?page=voters/register">New to this site?</a>
+                        </div>
+
+                        <!-- Login btn -->
+                        <div class="form-group my-3">
+                            <input type="submit" class="btn w-100 text-center" value="Login">
+                        </div>
+                    </form>
+            </div>
+        </div>
+    </div>
+</div>
